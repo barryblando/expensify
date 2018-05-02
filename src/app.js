d85b98588775a0e8d92fbd2bbfb23d3006ad48df
Retr0_0x315 <barryblando@gmail.com>
2018-05-02 16:27:02 +0800

    Update React Boilerplate.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
  // render IndecisionApp & its Components & sub Components to REACT DOM
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>BoilerPlate</p>, document.getElementById('app'));
 // ReactDOM.render(<IndecisionApp options={[]}/>, document.getElementById('app'));
 
-export default hot(module)(IndecisionApp); // for React hot loader
+export default hot(module)(<p>BoilerPlate</p>); // for React hot loader
