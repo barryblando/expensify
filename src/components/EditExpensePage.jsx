b1b5a0112e0954996b459ec9112af10a6fab1b32
Retr0_0x315 <barryblando@gmail.com>
2018-05-13 21:02:20 +0800

    formatted components and style code according to eslint & stylelint.

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = (props) => {
+const EditExpensePage = props => {
   // React Router pass props object to components
   // so that it can be manipulate dynamically
   console.log(props);
-  return (
-    <div>
-      Editing the expense with id of { props.match.params.id }
-    </div>
-  );
+  return <div>Editing the expense with id of {props.match.params.id}</div>;
 };
 
 export default EditExpensePage;
