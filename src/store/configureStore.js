7eda33a4641498611e17a357170b4433817d69ef
Retr0_0x315 <barryblando@gmail.com>
2018-05-21 20:13:12 +0800

    Added redux store & React Hot Loader for redux Provider.

---
+++ src/store/configureStore.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+
+// ------------------------------------------
+// Store Creation using combineReducer
+// for Multiple Reducers, when dispatching
+// its gonna be dispatch to both reducers,
+// but use switch to separate it by type
+// ------------------------------------------
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+    })
+  );
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept([expensesReducer, filtersReducer], () => {
+      const nextRootReducer = combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer,
+      });
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  // when importing reducers and calling export default then return store
+  return store;
+};
