f048bb6d69814552004835a7df207e82dce2dcab
Retr0_0x315 <barryblando@gmail.com>
2018-05-23 19:07:51 +0800

    Update Expense List Item.

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
-// When you connect a component to redux store its reactive
-// When the store changes the component gets re-rendered w/ new values
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
       {amount} - {createdAt}
     </p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-// no state, Just access the dispatch via connect
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
