93186b0aacc37c16e0854e011060bc36721b01fa
Retr0_0x315 <barryblando@gmail.com>
2018-05-23 20:04:27 +0800

    Added Constants values.

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const DISPLAY_FORMAT = 'L';
+export const ISO_FORMAT = 'YYYY-MM-DD';
+export const ISO_MONTH_FORMAT = 'YYYY-MM';
+
+export const START_DATE = 'startDate';
+export const END_DATE = 'endDate';
