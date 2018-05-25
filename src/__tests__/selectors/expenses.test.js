da263af057eedbfa0194d6b1dfe5246d730d68d9
Retr0_0x315 <barryblando@gmail.com>
2018-05-25 11:02:19 +0800

    Moved expense array object to fixture.

--- src/__tests__/selectors/expenses.test.js
+++ src/__tests__/selectors/expenses.test.js
@@ -1,34 +1,7 @@
 import selectExpenses from '../../selectors/expenses';
+import expenses from '../fixtures/expenses';
 
 const moment = require('moment');
-
-const expenses = [
-  {
-    id: '1',
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0,
-  },
-  {
-    id: '2',
-    description: 'Rent',
-    note: '',
-    amount: 1095000,
-    createdAt: moment(0)
-      .subtract(4, 'days')
-      .valueOf(),
-  },
-  {
-    id: '3',
-    description: 'Credit Card',
-    note: '',
-    amount: 4500,
-    createdAt: moment(0)
-      .add(4, 'days')
-      .valueOf(),
-  },
-];
 
 test('should filter by text value', () => {
   const filters = {
