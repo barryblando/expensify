769d423fed36e3d1406426f362c197f65a2a1245
Retr0_0x315 <barryblando@gmail.com>
2018-05-29 11:19:51 +0800

    Refactor Add & Edit Expense Page to Class Components for mapDispatchToProps.

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
 // When you connect a component to redux store its reactive
 // When the store changes the component gets re-rendered w/ new values
-const AddExpensePage = props => (
-  <div>
-    <h1>Add Expense</h1>
-    {/* Pass prop onSubmit to ExpenseForm */}
-    <ExpenseForm
-      onSubmit={expense => {
-        props.dispatch(addExpense(expense));
-        // go back to dashboard by Accessing props from react router
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+// export the unconnected component for Testing
+export class AddExpensePage extends Component {
+  onSubmit = expense => {
+    this.props.addExpense(expense);
+    // go back to dashboard by Accessing props from react router
+    this.props.history.push('/');
+  };
 
-export default connect()(AddExpensePage);
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        {/* Pass prop onSubmit to ExpenseForm */}
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+// pass this dispatch as prop on Add Expense
+// This is the way to return dispatcher function, allowing to obstruct them away from the component itself
+const mapDispatchToProps = dispatch => ({
+  addExpense: expense => dispatch(addExpense(expense)),
+});
+
+// set mapStateToProps to undefined 'cause we're not using it
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -1,43 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = props => {
-  // React Router pass props object to components
-  // so that it can be manipulate dynamically
-  console.log(props);
-  // return <div>Editing the expense with id of {props.match.params.id}</div>;
-  return (
-    <div>
-      <h1>Edit Expense</h1>
-      {/* Pass prop expense & onSubmit to ExpenseForm */}
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={expense => {
-          // Dispatch the action to edit the expense
-          props.dispatch(editExpense(props.expense.id, expense));
-          console.log('updated', expense);
-          // Redirect to the dashboard
-          props.history.push('/');
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push('/');
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+export class EditExpensePage extends Component {
+  onSubmit = expense => {
+    // Dispatch the action to edit the expense
+    this.props.editExpense(this.props.expense.id, expense);
+    console.log('updated:', expense);
+    // Redirect to the dashboard
+    this.props.history.push('/');
+  };
+
+  onRemove = () => {
+    this.props.removeExpense({ id: this.props.expense.id }); // data
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Edit Expense</h1>
+        {/* Pass prop expense & onSubmit to ExpenseForm */}
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onRemove}>Remove</button>
+      </div>
+    );
+  }
+}
+
+// const EditExpensePage = props => {
+//   // React Router pass props object to components
+//   // so that it can be manipulate dynamically
+//   console.log(props);
+//   // return <div>Editing the expense with id of {props.match.params.id}</div>;
+//   return (
+//     <div>
+//       <h1>Edit Expense</h1>
+//       {/* Pass prop expense & onSubmit to ExpenseForm */}
+//       <ExpenseForm
+//         expense={props.expense}
+//         onSubmit={expense => {
+//           // Dispatch the action to edit the expense
+//           props.dispatch(editExpense(props.expense.id, expense));
+//           console.log('updated', expense);
+//           // Redirect to the dashboard
+//           props.history.push('/');
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           props.dispatch(removeExpense({ id: props.expense.id }));
+//           props.history.push('/');
+//         }}
+//       >
+//         Remove
+//       </button>
+//     </div>
+//   );
+// };
 
 // Find the expense that needs to update, access props that are passed in by HOC
 const mapStateToProps = (state, props) => ({
   expense: state.expenses.find(expense => expense.id === props.match.params.id), // if found put it to expense
 });
 
+// Pass dispatchers as props
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: data => dispatch(removeExpense(data)),
+});
+
 // React Router render HOC then passes the props through and allows to add new ones
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
