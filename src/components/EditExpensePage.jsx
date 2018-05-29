26e1b3e2cfc3eed14d94c0eff8067ddd93e25402
Retr0_0x315 <barryblando@gmail.com>
2018-05-29 11:21:37 +0800

    Refactor Add & Edit Expense Page to Class Components & its event to  methods for mapDispatchToProps.

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -4,6 +4,8 @@ import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends Component {
+  // React Router pass props object to components
+  // so that it can be manipulate dynamically
   onSubmit = expense => {
     // Dispatch the action to edit the expense
     this.props.editExpense(this.props.expense.id, expense);
@@ -28,37 +30,6 @@ export class EditExpensePage extends Component {
     );
   }
 }
-
-// const EditExpensePage = props => {
-//   // React Router pass props object to components
-//   // so that it can be manipulate dynamically
-//   console.log(props);
-//   // return <div>Editing the expense with id of {props.match.params.id}</div>;
-//   return (
-//     <div>
-//       <h1>Edit Expense</h1>
-//       {/* Pass prop expense & onSubmit to ExpenseForm */}
-//       <ExpenseForm
-//         expense={props.expense}
-//         onSubmit={expense => {
-//           // Dispatch the action to edit the expense
-//           props.dispatch(editExpense(props.expense.id, expense));
-//           console.log('updated', expense);
-//           // Redirect to the dashboard
-//           props.history.push('/');
-//         }}
-//       />
-//       <button
-//         onClick={() => {
-//           props.dispatch(removeExpense({ id: props.expense.id }));
-//           props.history.push('/');
-//         }}
-//       >
-//         Remove
-//       </button>
-//     </div>
-//   );
-// };
 
 // Find the expense that needs to update, access props that are passed in by HOC
 const mapStateToProps = (state, props) => ({
