6dfb8ba6a3420556e267f9d7e9f7d2f0c20a2bf1
Retr0_0x315 <barryblando@gmail.com>
2018-05-30 14:23:41 +0800

    access redux store.

--- src/app.js
+++ src/app.js
@@ -8,24 +8,14 @@ import { hot } from 'react-hot-loader';
 
 // App, Style, Reset
 import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css'; // import default style for date picker
 import AppRouter from './routers/AppRouter';
 import './styles/style.scss';
 
 // State Management (Redux), actions
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore(); // now can use dispatch, getState, & subscribe
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Electricity Bill', amount: 50, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Car Rent', amount: 109500, createdAt: 2000 }));
-store.dispatch(addExpense({ description: 'Car Expense', amount: 109500, createdAt: 2000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 // To access redux store we gonna use Provider
 const app = (
@@ -34,9 +24,7 @@ const app = (
   </Provider>
 );
 
-// render App & its Components & sub Components to REACT DOM
 render(app, document.getElementById('app'));
-// ReactDOM.render(<IndecisionApp options={[]}/>, document.getElementById('app'));
 
 // export default hot(module)(<AppRouter />); // for React hot loader
 
