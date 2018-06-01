8536a69d42ca193252bfb1f055505a7ce053606d
Retr0_0x315 <barryblando@gmail.com>
2018-06-01 11:16:01 +0800

    Setup formatting for amount & date.

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import { ISO_DATE_FORMAT, CURRENCY_FORMAT } from '../constants';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
@@ -7,7 +10,9 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format(CURRENCY_FORMAT)}
+      -
+      {moment(createdAt).format(ISO_DATE_FORMAT)}
     </p>
   </div>
 );
