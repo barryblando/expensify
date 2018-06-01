6861a7686b7d159b992701d8d46a597c565d6853
Retr0_0x315 <barryblando@gmail.com>
2018-06-01 11:16:39 +0800

    Added Date & Numeric Formats.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 export const DISPLAY_FORMAT = 'L';
-export const ISO_FORMAT = 'YYYY-MM-DD';
+export const ISO_DATE_FORMAT = 'MMMM Do, YYYY'; // Moment Display format
 export const ISO_MONTH_FORMAT = 'YYYY-MM';
 
 export const START_DATE = 'startDate';
 export const END_DATE = 'endDate';
+
+export const CURRENCY_FORMAT = '$0,0.00'; // Numeral Display format
