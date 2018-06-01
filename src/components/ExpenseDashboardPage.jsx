715e384ca22dc5cf90d5ef3e2b7aa83a385a788c
Retr0_0x315 <barryblando@gmail.com>
2018-06-01 14:38:12 +0800

    Added and rendered Expense Summary to Dashboard Page.

--- src/components/ExpenseDashboardPage.jsx
+++ src/components/ExpenseDashboardPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList'; /* eslint-disable-line */
 import ExpenseListFilters from './ExpenseListFilters'; /* eslint-disable-line */
+import ExpensesSummary from './ExpensesSummary'; /* eslint-disable-line */
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
