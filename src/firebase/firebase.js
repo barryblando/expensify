44e8ef252a319db9ce6963193d7efb5d6eeebf5d
Retr0_0x315 <barryblando@gmail.com>
2018-06-05 18:46:05 +0800

    Initialized Firebase.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyB23tI5T6eZVDe7yU8uVUPvmj7h-JVYPCo',
+  authDomain: 'expensify-1195c.firebaseapp.com',
+  databaseURL: 'https://expensify-1195c.firebaseio.com',
+  projectId: 'expensify-1195c',
+  storageBucket: 'expensify-1195c.appspot.com',
+  messagingSenderId: '697096536530',
+};
+
+firebase.initializeApp(config);
