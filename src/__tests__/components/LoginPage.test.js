7c5a902fade7ee2dc1cda522062a2463049607e8
Retr0_0x315 <barryblando@gmail.com>
2018-06-13 12:56:47 +0800

    Added LoadingPage Test and Fixed LoginPage cache.

--- src/__tests__/components/LoginPage.test.js
+++ src/__tests__/components/LoginPage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
-test('should render 404 correctly', () => {
+test('should render Login Page correctly', () => {
   const wrapper = shallow(<LoginPage />);
   expect(wrapper).toMatchSnapshot();
 });
