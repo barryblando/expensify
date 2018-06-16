e0eaf1ae842e02db5666650033be5fef2e96ee4c
Retr0_0x315 <barryblando@gmail.com>
2018-06-16 21:21:27 +0800

    Added Add, Edit, & Dashboard Income Page.

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,9 +4,13 @@ import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter } from 'connected-react-router';
 
 // Components
+import DashboardPage from '../components/DashboardPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import IncomeDashboardPage from '../components/IncomeDashboardPage';
 import AddExpensePage from '../components/AddExpensePage'; // eslint-disable-line
+import AddIncomePage from '../components/AddIncomePage'; // eslint-disable-line
 import EditExpensePage from '../components/EditExpensePage'; // eslint-disable-line
+import EditIncomePage from '../components/EditIncomePage'; // eslint-disable-line
 import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage'; // eslint-disable-line
 import NotFoundPage from '../components/404';
@@ -26,10 +30,14 @@ const AppRouter = () => (
     <div>
       <Switch>
         <PublicRoute path="/" component={LoginPage} exact />
-        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/dashboard" component={DashboardPage} />
+        <PrivateRoute path="/expense-dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/income-dashboard" component={IncomeDashboardPage} />
+        <PrivateRoute path="/create-expense" component={AddExpensePage} />
+        <PrivateRoute path="/create-income" component={AddIncomePage} />
         {/* This is the same as express routes, :id will be pass as props.match.params key in EditExpensePage dynamically */}
-        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/edit-expense/:id" component={EditExpensePage} />
+        <PrivateRoute path="/edit-income/:id" component={EditIncomePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
