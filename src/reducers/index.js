e54c354db70d89ca5ca0f1b9f18467d5c7320b7c
Retr0_0x315 <barryblando@gmail.com>
2018-06-18 19:49:24 +0800

    Added Income Reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import expensesReducer from './expenses';
+import incomesReducer from './incomes';
 import filtersReducer from './filters';
 import authReducer from './auth';
 
 const rootReducer = combineReducers({
   expenses: expensesReducer,
+  incomes: incomesReducer,
   filters: filtersReducer,
   auth: authReducer,
 });
