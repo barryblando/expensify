ae1d4f74e071f8a92cfeeeaa2b522d2deb9e43e2
Retr0_0x315 <barryblando@gmail.com>
2018-06-18 19:47:30 +0800

    Added Income selectors.

---
+++ src/selectors/incomes.js
@@ -0,0 +1,34 @@
+import moment from 'moment'; // temp fix for moment not working when using es6 modules
+
+// ------------------------------------------
+// GET VISIBLE INCOMES
+// ------------------------------------------
+
+export default (incomes, { text, sortBy, startDate, endDate }) =>
+  // Filtering out
+  incomes
+    .filter(income => {
+      // -------- LOGIC START --------
+      // if startDate or endDate is a number then proceed to next condition (otherwise won't be filtered)
+      // if createdAt is greater than startDate or less than endDate, do included in filter expenses (otherwise filtered out)
+      // and if expenses.description has the text variable string inside of it (lowercase sensitive)
+      // -------- LOGIC END --------
+      // const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+      // const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+      const createdAtMoment = moment(income.createdAt);
+      const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+      const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+      const textMatch = income.description.toLowerCase().includes(text.toLowerCase());
+
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      let sortedValue;
+      if (sortBy === 'date') {
+        // <reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+        sortedValue = a.createdAt < b.createdAt ? 1 : -1; // if true return b/1 else a/-1
+      } else if (sortBy === 'amount') {
+        sortedValue = a.amount < b.amount ? 1 : -1;
+      }
+      return sortedValue;
+    });
