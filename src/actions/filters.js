e27cb61b2dba9d6d1390e3c9de28d1f8549684e4
Retr0_0x315 <barryblando@gmail.com>
2018-06-22 20:20:27 +0800

    Refactored types value.

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,27 +1,29 @@
+import { SET_TEXT_FILTER, SORT_BY_AMOUNT, SORT_BY_DATE, SET_START_DATE, SET_END_DATE } from '../constants';
+
 // -- SET_TEXT_FILTER --
 export const setTextFilter = (text = '') => ({
-  type: 'SET_TEXT_FILTER',
+  type: SET_TEXT_FILTER,
   text,
 });
 
 // -- SORT_BY_AMOUNT --
 export const sortByAmount = () => ({
-  type: 'SORT_BY_AMOUNT',
+  type: SORT_BY_AMOUNT,
 });
 
 // -- SORT_BY_DATE --
 export const sortByDate = () => ({
-  type: 'SORT_BY_DATE',
+  type: SORT_BY_DATE,
 });
 
 // -- SET_START_DATE --
 export const setStartDate = startDate => ({
-  type: 'SET_START_DATE',
+  type: SET_START_DATE,
   startDate,
 });
 
 // -- SET_END_DATE --
 export const setEndDate = endDate => ({
-  type: 'SET_END_DATE',
+  type: SET_END_DATE,
   endDate,
 });
