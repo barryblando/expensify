4453e8afaf25cd9371d3b0e2a1ef18504d55f70d
Retr0_0x315 <barryblando@gmail.com>
2018-07-02 14:00:40 +0800

    update info on expenses reducer.

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -8,24 +8,24 @@ const expenseReducerDefaultState = [];
 export default (state = expenseReducerDefaultState, action) => {
   switch (action.type) {
     case ADD_EXPENSE:
-      return [...state, action.expense]; // using spread op same result as concat creating new array state rather than changing the prevState
+      // using spread op same result as concat creating new array state rather than changing the prevState
+      return [...state, action.expense];
     case REMOVE_EXPENSE:
-      // state.map(expense => console.log(expense)); destruct expense array object only id in filter
-      return state.filter(({ id }) => id !== action.id); // filtered out if not match
+      // destruct expense array object only id in filter, filtered out if not match
+      return state.filter(({ id }) => id !== action.id);
     case EDIT_EXPENSE:
       return state.map(expense => {
         // check if current expense iterating over by map match to action id
         if (expense.id === action.id) {
-          // return brand new object & by using object spread operator
+          // return brand new object by using object spread operator
           return {
-            ...expense, // grab all existing properties
-            ...action.updates, // override any of the ones in expense that are pass down by updates
+            ...expense, // grab all current properties in expense object
+            ...action.updates, // override the ones in current expense object that are pass down by updates
           };
         }
         return expense; // if no changes return
       });
     case SET_EXPENSES:
-      // console.log(state);
       return action.expenses;
     default:
       return state;
