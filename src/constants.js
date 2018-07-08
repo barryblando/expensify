15de272b23d46a351fb2927f6d7ddd813c1c8549
Retr0_0x315 <barryblando@gmail.com>
2018-07-08 14:00:15 +0800

    remove actions.

--- src/constants.js
+++ src/constants.js
@@ -6,23 +6,3 @@ export const START_DATE = 'startDate';
 export const END_DATE = 'endDate';
 
 export const CURRENCY_FORMAT = '$0,0.00'; // Numeral Display format
-
-// ACTIONS
-export const ADD_EXPENSE = 'ADD_EXPENSE';
-export const EDIT_EXPENSE = 'EDIT_EXPENSE';
-export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
-export const SET_EXPENSES = 'SET_EXPENSES';
-
-export const ADD_INCOME = 'ADD_INCOME';
-export const EDIT_INCOME = 'EDIT_INCOME';
-export const REMOVE_INCOME = 'REMOVE_INCOME';
-export const SET_INCOMES = 'SET_INCOMES';
-
-export const SET_TEXT_FILTER = 'SET_TEXT_FILTER';
-export const SORT_BY_AMOUNT = 'SORT_BY_AMOUNT';
-export const SORT_BY_DATE = 'SORT_BY_DATE';
-export const SET_START_DATE = 'SET_START_DATE';
-export const SET_END_DATE = 'SET_END_DATE';
-
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
