3e09db9d35a6b4fccfab0eb949e931896e84d5da
Retr0_0x315 <barryblando@gmail.com>
2018-07-21 20:54:39 +0800

    update filter reducer tests.

--- src/__tests__/reducers/filters.test.js
+++ src/__tests__/reducers/filters.test.js
@@ -4,37 +4,54 @@ import filtersReducer from '../../redux/reducers/filters';
 test('should setup default filter values', () => {
   const state = filtersReducer(undefined, { type: '@@INIT' });
   expect(state).toEqual({
-    text: '',
-    sortBy: 'date',
-    startDate: moment().startOf('month'),
-    endDate: moment().endOf('month'),
+    expenses: {
+      text: '',
+      sortBy: 'date',
+      startDate: moment().startOf('month'),
+      endDate: moment().endOf('month'),
+    },
+    incomes: {
+      text: '',
+      sortBy: 'amount',
+      startDate: moment().startOf('month'),
+      endDate: moment().endOf('month'),
+    },
   });
 });
 
 test('should set sortBy to amount', () => {
-  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
-  expect(state.sortBy).toBe('amount');
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT', filterType: 'expenses' });
+  expect(state.expenses.sortBy).toBe('amount');
 });
 
 test('should set sortBy to date', () => {
   const currentState = {
-    text: '',
-    startDate: undefined,
-    endDate: undefined,
-    sortBy: 'amount',
+    expenses: {
+      text: '',
+      sortBy: 'date',
+      startDate: undefined,
+      endDate: undefined,
+    },
+    incomes: {
+      text: '',
+      sortBy: 'amount',
+      startDate: undefined,
+      endDate: undefined,
+    },
   };
-  const action = { type: 'SORT_BY_DATE' };
+  const action = { type: 'SORT_BY_DATE', filterType: 'incomes' };
   const state = filtersReducer(currentState, action); // expect sortBy amount to change to date by passing action
-  expect(state.sortBy).toBe('date');
+  expect(state.incomes.sortBy).toBe('date');
 });
 
 test('should set text filter', () => {
   const action = {
     type: 'SET_TEXT_FILTER',
     text: 'Rent',
+    filterType: 'expenses',
   };
   const state = filtersReducer(undefined, action);
-  expect(state.text).toBe('Rent');
+  expect(state.expenses.text).toBe('Rent');
 });
 
 test('should set startDate filter', () => {
@@ -42,9 +59,10 @@ test('should set startDate filter', () => {
   const action = {
     type: 'SET_START_DATE',
     startDate,
+    filterType: 'incomes',
   };
   const state = filtersReducer(undefined, action);
-  expect(state.startDate).toEqual(startDate);
+  expect(state.incomes.startDate).toEqual(startDate);
 });
 
 test('should set enDate filter', () => {
@@ -52,7 +70,8 @@ test('should set enDate filter', () => {
   const action = {
     type: 'SET_END_DATE',
     endDate,
+    filterType: 'expenses',
   };
   const state = filtersReducer(undefined, action);
-  expect(state.endDate).toEqual(endDate);
+  expect(state.expenses.endDate).toEqual(endDate);
 });
