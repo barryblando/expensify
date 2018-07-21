37a9b09357f246ac978b60e4b9e284eb5c7d2fbd
Retr0_0x315 <barryblando@gmail.com>
2018-07-21 20:48:58 +0800

    update  action filter tests

--- src/__tests__/actions/filters.test.js
+++ src/__tests__/actions/filters.test.js
@@ -2,26 +2,29 @@ import moment from 'moment';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../../redux/actions/filters';
 
 test('should generate set start date action object', () => {
-  const action = setStartDate(moment(0));
+  const action = setStartDate(moment(0), 'expenses');
   expect(action).toEqual({
     type: 'SET_START_DATE',
     startDate: moment(0),
+    filterType: 'expenses',
   });
 });
 
 test('should generate set end date action object', () => {
-  const action = setEndDate(moment(0));
+  const action = setEndDate(moment(0), 'incomes');
   expect(action).toEqual({
     type: 'SET_END_DATE',
     endDate: moment(0),
+    filterType: 'incomes',
   });
 });
 
 test('should generate set text filter action object', () => {
-  const action = setTextFilter('Rent');
+  const action = setTextFilter('Rent', 'expenses');
   expect(action).toEqual({
     type: 'SET_TEXT_FILTER',
     text: 'Rent',
+    filterType: 'expenses',
   });
 });
 
@@ -30,13 +33,20 @@ test('should generate set text filter action object with default', () => {
   expect(action).toEqual({
     type: 'SET_TEXT_FILTER',
     text: '',
+    filterType: undefined,
   });
 });
 
 test('should generate sort by amount action object', () => {
-  expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
+  expect(sortByAmount('expenses')).toEqual({
+    type: 'SORT_BY_AMOUNT',
+    filterType: 'expenses',
+  });
 });
 
 test('should generate sort by date action object', () => {
-  expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
+  expect(sortByDate('incomes')).toEqual({
+    type: 'SORT_BY_DATE',
+    filterType: 'incomes',
+  });
 });
